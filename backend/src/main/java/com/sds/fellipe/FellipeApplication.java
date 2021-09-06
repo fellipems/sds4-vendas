b3d47ddcfc43c9d166797d85f188ffb3eeeb10e3
fellipems <fellipems14@gmail.com>
2021-09-06 08:59:16 -0300

    project created

---
+++ backend/src/main/java/com/sds/fellipe/FellipeApplication.java
@@ -0,0 +1,13 @@
+package com.sds.fellipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FellipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FellipeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sds/fellipe/FellipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.fellipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FellipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
